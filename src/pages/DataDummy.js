1427a77cf6ad99a4382df9bdc279cfe68dec576e
RyanAR <ryar.7truth@gmail.com>
2023-07-20 14:32:16 +0700

    Add All Camera Page

---
+++ src/pages/DataDummy.js
@@ -0,0 +1,72 @@
+export const cameraData = [
+  {
+    title: 'SP-NG-KMFD-04',
+    type: 'Sholat',
+    id: 1,
+  },
+  {
+    title: 'SP-NG-KMFD-05',
+    type: 'Sholat',
+    id: 2,
+  },
+  {
+    title: 'SP-NG-KMFD-06',
+    type: 'Sholat',
+    id: 3,
+  },
+  {
+    title: 'SP-NG-KAMMAKS-50',
+    type: 'Sholat',
+    id: 4,
+  },
+  {
+    title: 'SP-NG-KAMMAKS-51',
+    type: 'Sholat',
+    id: 5,
+  },
+  {
+    title: 'SP-NG-KAMMAKS-52',
+    type: 'Sholat',
+    id: 6,
+  },
+  {
+    title: 'SP-NG-KMFD-04',
+    type: 'Perkelahian',
+    id: 7,
+  },
+  {
+    title: 'SP-NG-KMFD-05',
+    type: 'Perkelahian',
+    id: 8,
+  },
+  {
+    title: 'SP-NG-KMFD-06',
+    type: 'Perkelahian',
+    id: 9,
+  },
+  {
+    title: 'SP-NG-KAMMAKS-50',
+    type: 'Perkelahian',
+    id: 10,
+  },
+  {
+    title: 'SP-NG-KAMMAKS-51',
+    type: 'Perkelahian',
+    id: 11,
+  },
+  {
+    title: 'SP-NG-KAMMAKS-52',
+    type: 'Perkelahian',
+    id: 12,
+  },
+  {
+    title: 'SP-NG-KAMMAKS-49',
+    type: 'Perkelahian',
+    id: 13,
+  },
+  {
+    title: 'SP-NG-KAMMAKS-47',
+    type: 'Perkelahian',
+    id: 14,
+  },
+]
