b677cf9f63dec30cbf0811651f23019bc9bc63b2
yusufvalid <103932140+yusufvalid@users.noreply.github.com>
2023-07-27 16:53:56 +0700

    created header and tabs

--- src/components/ThemeReplacementWidget/ThemeReplacementWidget.jsx
+++ src/components/ThemeReplacementWidget/ThemeReplacementWidget.jsx
@@ -1,37 +1,69 @@
+import { useState } from 'react'
+
+// CONSTANTS
+import { colors } from 'constants/colors'
+
 //MUIS
-import { IconButton, Stack } from '@mui/material'
+import { IconButton, Stack, Tab, Tabs } from '@mui/material'
 
 // MUI ICONS
 import SettingsIcon from '@mui/icons-material/Settings'
 
 const ThemeReplacementWidget = () => {
+  const [tabValue, setTabValue] = useState(0)
+  const [isWidgetExpanded, setIsWidgetExpanded] = useState(false)
   return (
     <Stack
-      width='300px'
       sx={{
+        width: '300px',
+        position: 'fixed',
         top: 130,
         right: 0,
+        transform: isWidgetExpanded ? 'translateX(0px)' : 'translateX(260px)',
+        transition: 'transform 0.5s',
       }}
-      position='fixed'
     >
       {/* HEADER */}
       <Stack
         width='100%'
         height='50px'
-        sx={{ backgroundColor: 'green' }}
+        sx={{
+          backgroundColor: 'white',
+          borderTopLeftRadius: '4px',
+          borderBottomLeftRadius: '4px',
+        }}
         direction='row'
-        borderRadius='4px'
       >
         {/* ICON */}
         <Stack
-          width='35px'
+          width='50px'
           height='50px'
           alignItems='center'
           justifyContent='center'
         >
-          <IconButton disableRipple>
-            <SettingsIcon />
+          <IconButton
+            disableRipple
+            onClick={() => setIsWidgetExpanded((prev) => !prev)}
+          >
+            <SettingsIcon sx={{ color: colors.info }} />
           </IconButton>
+        </Stack>
+        {/* TABS */}
+        <Stack
+          width='100%'
+          height='50px'
+          justifyContent='center'
+          alignItems='center'
+        >
+          <Tabs
+            value={tabValue}
+            onChange={(event, newValue) => setTabValue(newValue)}
+            sx={{ '& .MuiButtonBase-root': { textTransform: 'none' } }}
+          >
+            <Tab label='Sidebar' />
+            <Tab label='Navbar' />
+            <Tab label='Primary' />
+          </Tabs>
         </Stack>
       </Stack>
     </Stack>
